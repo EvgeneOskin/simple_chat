ab5817f6ec9506b21576581b3c98d773db7788e1
Evgene Oskin <burnst@mail.ru>
2012-06-25 11:59:05 +0700

    server

---
+++ connector_server.cpp
@@ -0,0 +1,60 @@
+#include "connector_server.h"
+
+
+Connector_Server::Connector_Server(int nPort)
+    : QObject(), connected_str(), NextBlockSize(0)
+{
+    connected_Server = new QTcpServer(this);
+    if (!connected_Server->listen(QHostAddress::Any, nPort))
+    {
+        QMessageBox::critical(0, "Server Error",
+                              "Enable ot start the server:"
+                              +connected_Server->errorString());
+        connected_Server->close();
+        return;
+    }
+    connect(connected_Server,SIGNAL(newConnection()),this,SLOT(slotNewConnection()));
+    qDebug() << connected_Server->serverAddress();
+}
+
+void Connector_Server::slotNewConnection()
+{
+    static unsigned int numbersOfConnections;
+    QTcpSocket *connected_ClientSocket = connected_Server->nextPendingConnection();
+//    connect(connected_ClientSocket, SIGNAL(disconnected()), this, SLOT(deleteLater()));
+    connect(connected_ClientSocket, SIGNAL(readyRead()), this, SLOT(slotReadClient()));
+    numbersOfConnections++;
+    qDebug() << "You have new connection! Server" << numbersOfConnections;
+}
+
+void Connector_Server::slotReadClient()
+{
+
+    QTcpSocket *connected_ClientSocket = (QTcpSocket*)sender();
+    QDataStream Read_stream(connected_ClientSocket);
+    Read_stream.setVersion(QDataStream::Qt_4_8);
+    connected_str.clear();
+
+    for(;;)
+    {
+        if (!NextBlockSize)
+        {
+            if (connected_ClientSocket->bytesAvailable() < sizeof(quint16))
+            {
+                break;
+            }
+        Read_stream >> NextBlockSize;
+        }
+
+        if (connected_ClientSocket->bytesAvailable() <= NextBlockSize)
+        {
+            break;
+        }
+    }
+
+    Read_stream >> connected_str;
+
+    emit NewOutput(connected_str);
+
+    NextBlockSize = 0;
+}

776f10eb8c170cf3dbbc875149dabf3a4273ea2c
Evgene Oskin <burnst@mail.ru>
2012-06-25 11:58:27 +0700

    client

---
+++ connector_client.cpp
@@ -0,0 +1,28 @@
+#include "connector_client.h"
+
+Connector_Client::Connector_Client(const QString &Host_Name, int nPort)
+    :QObject()
+{
+    connected_Socket = new QTcpSocket(this);
+    connected_Socket->connectToHost(Host_Name,nPort);
+    connect(connected_Socket, SIGNAL(connected()),SLOT(slotConnected()));
+}
+
+void Connector_Client::slotSendToServer(QString send_str)
+{
+    QByteArray Send_Block;
+    QDataStream Send_stream(&Send_Block, QIODevice::WriteOnly);
+    Send_stream.setVersion(QDataStream::Qt_4_8);
+    Send_stream << quint16(0) << send_str;
+
+    Send_stream.device()->seek(0);
+    Send_stream << quint16(Send_Block.size() - sizeof(quint16));
+
+    connected_Socket->write(Send_Block);
+//    qDebug() << send_str;
+}
+
+void Connector_Client::slotConnected()
+{
+    qDebug() << "You are connected... client";
+}

b6444464a5f7f80d9809cd76fbaa68fed87ea232
Evgene Oskin <burnst@mail.ru>
2012-06-26 14:20:46 +0700

    client part header

--- connector_client.h
+++ connector_client.h
@@ -11,10 +11,13 @@ class Connector_Client : public QObject
 {
     Q_OBJECT
 private:
+    bool connect_INDEX;
     QTcpSocket *connected_Socket;
 
 public:
     Connector_Client(const QString &Host_Name, int nPort);
+    ~Connector_Client();
+    bool isConnectedToHost();
 
 signals:
     void connection_lost();
@@ -22,6 +25,7 @@ signals:
 public slots:
     void slotSendToServer(QString);
     void slotConnected();
+    void slotDisconnected();
 };
 
 #endif // CONNECTOR_CLIENT_H

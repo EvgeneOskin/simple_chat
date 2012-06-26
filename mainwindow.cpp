2b3e1652a1a6a00f73b445354ce76fb91887aba5
Evgene Oskin <burnst@mail.ru>
2012-06-26 14:24:01 +0700

    correctly react to disconnection

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,11 +3,12 @@
 MainWindow::MainWindow()
     :QWidget()
 {
-    comment_label = new QLabel("It's for your reciever IP");
+    comment_label = new QLabel("It's for your reciever IP or host name");
+    connection_label = new QLabel("Disconected!");
     out_label = new QLabel("It's for output");
     in_label = new QLabel("It's for  input");
     in_connection_name = new QLineEdit();
-    out_text = new QTextEdit("Here you will see what input");
+    out_text = new QTextEdit("Here you will receive messages");
     input_text = new QTextEdit();
     send_PshBttn = new QPushButton("Send");
     exit_PshBttn = new QPushButton("Exit");
@@ -40,6 +41,7 @@ MainWindow::MainWindow()
     total_Layout->addWidget(comment_label, 0);
     total_Layout->addWidget(in_connection_name, 0);
     total_Layout->addLayout(IOput_Layout, 2);
+    total_Layout->addWidget(connection_label, 0);
 
     setLayout(total_Layout);
 
@@ -48,8 +50,9 @@ MainWindow::MainWindow()
     QObject::connect(this, SIGNAL(NewInput_Self(QString)), C_connector, SLOT(slotSendToServer(QString)));
     QObject::connect(S_connector, SIGNAL(NewOutput(QString)), out_text, SLOT(setText(QString)));
     QObject::connect(exit_PshBttn, SIGNAL(clicked()), this, SIGNAL(close_MainWindow()));
-    QObject::connect(C_connector, SIGNAL(connection_lost()), this, SIGNAL(close_MainWindow()));
+    QObject::connect(C_connector, SIGNAL(connection_lost()), this, SLOT(slotServer_disconnected()));
 
+    slotServer_disconnected();
 }
 
 
@@ -70,11 +73,41 @@ void MainWindow::slotRead_Conection_Name()
     delete C_connector;
     C_connector = new Connector_Client(Connection_Name,2323);
     QObject::connect(this, SIGNAL(NewInput_Self(QString)), C_connector, SLOT(slotSendToServer(QString)));
+    QObject::connect(C_connector, SIGNAL(connection_lost()), this, SLOT(slotServer_disconnected()));
+    QTimer::singleShot(3000, this, SLOT(slotCheckConnection()));
 }
 
-void MainWindow::Server_disconnected()
+void MainWindow::slotServer_disconnected()
 {
-    out_text->setText("Sorry, I\'m lost connection with Server");
+    connection_label->setText("Disconected!");
     input_text->setText("Sorry, I\'m lost connection with Server");
     input_text->setReadOnly(true);
+    bool bOk;
+    QString Connection_Name = QInputDialog::getText(this, "host name",
+                                                    "Sorry, i lost current connection\nPlease input new host name (or IP):",
+                                                    QLineEdit::Normal, "", &bOk);
+    if(bOk)
+    {
+        C_connector->disconnect();
+        if (C_connector->isConnectedToHost())
+            delete C_connector;
+        C_connector = new Connector_Client(Connection_Name,2323);
+        QObject::connect(this, SIGNAL(NewInput_Self(QString)), C_connector, SLOT(slotSendToServer(QString)));
+        QObject::connect(C_connector, SIGNAL(connection_lost()), this, SLOT(slotServer_disconnected()));
+        QTimer::singleShot(3000, this, SLOT(slotCheckConnection()));
+    }
+}
+
+void MainWindow::slotCheckConnection()
+{
+    if (!C_connector->isConnectedToHost())
+        slotServer_disconnected();
+    else
+    {
+        connection_label->setText("Conected!");
+        out_text->setText("");
+        input_text->setText("");
+        input_text->setReadOnly(false);
+    }
+
 }
